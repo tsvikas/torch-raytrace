ce9f72fc82d15e30f0dcd1c8db68e9b23bfd670d
Tsvika Shapira <tsvikas@gmail.com>
2025-04-03 05:10:48 +0300

    add import to __init__

--- src/raytrace/__init__.py
+++ src/raytrace/__init__.py
@@ -3,7 +3,11 @@
 Copyright (c) 2025 Tsvika Shapira. All rights reserved.
 """
 
+from . import plot_render, ray_tracing
 from ._version import version as _version
 
 __version__ = _version
-__all__: list[str] = []
+__all__: list[str] = [
+    "plot_render",
+    "ray_tracing",
+]
