ea979f7e27bfb8ed544a2dc0195d221ccbf55155
Tsvika Shapira <tsvikas@gmail.com>
2025-04-03 05:38:14 +0300

    fixup! feat: ray directions

--- src/raytrace/plot_render.py
+++ src/raytrace/plot_render.py
@@ -100,6 +100,7 @@ def plot_3d(
     lines: Float[torch.Tensor, "lines p2 xyz"],
     points: Float[torch.Tensor, "points xyz"] | None = None,
     ax: Axes | None = None,
+    axis: Literal["yz", "zx", "xy"] = "yz",
 ) -> Axes:
     """Plot line segments and points in 3D."""
     if ax is None:
@@ -112,8 +113,20 @@ def plot_3d(
     if points is not None:
         points = points.cpu()
         ax.scatter(points[:, 2], points[:, 0], points[:, 1], c=points[:, 0])
-    scale = torch.linspace(-10, 10, 100)
-    ax.scatter(0, scale, 0, c=scale)
+
+    axis1 = ord(axis[0]) - ord("x")
+    axis2 = ord(axis[1]) - ord("x")
+    axis0 = 3 - axis1 - axis2
+    min0, max0 = lines[0, :, axis0]
+    scale = torch.linspace(min0, max0, 100)
+    if "z" not in axis:
+        ax.scatter(scale, 0, 0, c=scale)
+    elif "x" not in axis:
+        ax.scatter(0, scale, 0, c=scale)
+    elif "y" not in axis:
+        ax.scatter(0, 0, scale, c=scale)
+    else:
+        raise ValueError(f"axis {axis} not supported")
 
     ax.set(xlabel="z", ylabel="x", zlabel="y")
     return ax
@@ -151,7 +164,14 @@ def plot_render(
 ) -> None:
     """Plot rays, triangles, and screen."""
     num_pixels_y, num_pixels_z, _p2, _xyz = rays.shape
-    _x1, y_limit, z_limit = rays[0, 0, 1].cpu()
+    if "x" not in axis:
+        _x1, y_limit, z_limit = rays[0, 0, 1].cpu()
+    elif "y" not in axis:
+        z_limit, _x1, y_limit = rays[0, 0, 1].cpu()
+    elif "z" not in axis:
+        y_limit, z_limit, _x1 = rays[0, 0, 1].cpu()
+    else:
+        raise ValueError(f"axis {axis} not supported")
 
     plot_3d(
         points=einops.rearrange(triangles, "triangles p3 xyz -> (triangles p3) xyz"),
@@ -159,6 +179,7 @@ def plot_render(
             rays[:: num_pixels_y // 5, :: num_pixels_z // 5],
             "y z p2 xyz -> (y z) p2 xyz",
         ),
+        axis=axis,
     )
     plot_2d(
         screen=screen,
