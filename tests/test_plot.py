1e384c42db19cfbd20172051c4195c4ccef7f94c
Tsvika Shapira <tsvikas@gmail.com>
2025-04-03 07:03:40 +0300

    tests: plot axes

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -1,3 +1,7 @@
+from typing import Literal
+
+import pytest
+
 from raytrace import plot_render
 
 
@@ -9,8 +13,9 @@ def test_render_snorlax() -> None:
     plot_render.render_snorlax(10)
 
 
-def test_plot_render() -> None:
-    plot_render.plot_render(*plot_render.render_pikachu(10))
+@pytest.mark.parametrize("axis", ["xy", "yz", "zx"])
+def test_plot_render(axis: Literal["yz", "zx", "xy"]) -> None:
+    plot_render.plot_render(*plot_render.render_pikachu(10, axis=axis))
 
 
 def test_pikachu_side() -> None:
